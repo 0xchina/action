15fb991f581a9ba995502e7799fde6b0ffa9a3af
boss <apsu@ninga.co>
2022-03-10 15:56:56 +0800

    s

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,7 @@
 FROM docker:stable
+RUN docker pull docker.elastic.co/elasticsearch/elasticsearch:8.0.0
+RUN docker pull mongo
+RUN docker pull redis
+RUN docker pull eclipse-mosquitto
 COPY entrypoint.sh /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -7,3 +7,5 @@ docker run -p 6379:6379 redis redis-server --requirepass yangpaopao
 docker run -p 1883:1883 -v $(pwd)/mosquitto/mosquitto.conf:/mosquitto/config/mosquitto.conf -v $(pwd)/mosquitto/mosquitto.conf:/mosquitto/config/mosquitto.conf -v $(pwd)/auth.json:/mosquitto/auth/dynamic-security.json eclipse-mosquitto
 
 docker run -p 9200:9200 -v $(pwd)/esca.crt:/usr/share/elasticsearch/config/certs/http_ca.crt -e ELASTIC_PASSWORD=yangpaopao docker.elastic.co/elasticsearch/elasticsearch:8.0.0
+
+echo "::set-output name=time::$time"
